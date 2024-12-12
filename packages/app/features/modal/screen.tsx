f465ae22543df6d88f1905008a74c45b68679986
Mike Allen <mikefacesny@gmail.com>
2024-12-12 16:58:13 -0500

    add modal screen

---
+++ packages/app/features/modal/screen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+function ModalScreen() {
+  return (
+    <View className="min-h-screen w-full  flex-1 items-center bg-white p-3 ">
+      <Text className="font-bold text-red-700">Modal Screen</Text>
+    </View>
+  )
+}
+
+
+export default ModalScreen
